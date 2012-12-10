afd7f7650f0b8abe7865324558f66282c18215c9
matta <matta@npt>
2012-12-10 16:39:24 +0000

    * Fixing link issue

--- NPLib/Tigress/TTigressData.h
+++ NPLib/Tigress/TTigressData.h
@@ -100,7 +100,7 @@ public: // Method and object to construct to fill the data event from a Tigress 
   void FillGe(unsigned short GeCloverNbr, unsigned short GeCrystalNbr , unsigned short GeSegmentNbr, int hit,TTigEventFragment* TigEvent);
   void FillBGO(unsigned short BGOCloverNbr, unsigned short BGOPmNbr , unsigned short BGOSegmentNbr, int hit,TTigEventFragment* TigEvent);
 
-  map< int,vector<int> > GetFSPC2Detector() const ;//!
+  map< int,vector<int> > GetFSPC2Detector() const {return m_FSPC2Detector;}//!
   
 private:// Key is the FSPC channel,
         // vector[0] is the Clover Nbr
