da00c943954b8b462d3b27d74bb14ad720a6815f
matta <matta@npt>
2013-02-14 11:43:10 +0000

    * removing fs2pc stuff from NPL - Sharc Tigress and Trifoil are now stand alone, no mixeup with the Unpacker (Triumf code) needs

--- NPLib/Tigress/TTigressData.h
+++ NPLib/Tigress/TTigressData.h
@@ -27,9 +27,6 @@
 #include <map>
 using namespace std ;
 
-// NPL
-#include "TTigEventFragment.h"
-
 // ROOT
 #include "TObject.h"
 
@@ -93,22 +90,6 @@ public:
 
   unsigned int GetMultiplicityGe()  {return fTIG_Ge_CloverNbr.size();}
   unsigned int GetMultiplicityBGO()  {return fTIG_BGO_CloverNbr.size();}
-
-public: // Method and object to construct to fill the data event from a Tigress DAQ event tree
-  void ReadFSPCFile(string FSPCPath);//!
-  void FillData(TTigEventFragment* TigEvent);//!
-  void FillGe(unsigned short GeCloverNbr, unsigned short GeCrystalNbr , unsigned short GeSegmentNbr, int hit,TTigEventFragment* TigEvent);
-  void FillBGO(unsigned short BGOCloverNbr, unsigned short BGOPmNbr , unsigned short BGOSegmentNbr, int hit,TTigEventFragment* TigEvent);
-
-  map< int,vector<int> > GetFSPC2Detector() const {return m_FSPC2Detector;}//!
-  
-private:// Key is the FSPC channel,
-        // vector[0] is the Clover Nbr
-        // vector[1] is the crystal Nbr
-        // vector[2] is the segment Nbr
-        // vector[3] is 0: Ge , 1: BGO
-  map< int,vector<int> > m_FSPC2Detector;//!
-
   
   ClassDef(TTigressData,1)  // TigressData structure
 };
