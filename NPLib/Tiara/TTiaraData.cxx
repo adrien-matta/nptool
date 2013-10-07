7a8b0b19e9feb91e3d67c146e0d848bb38648892
adrien-matta <a.matta@surrey.ac.uk>
2013-10-07 15:54:16 +0100

    working on TTiaraData

--- NPLib/Tiara/TTiaraData.cxx
+++ NPLib/Tiara/TTiaraData.cxx
@@ -42,55 +42,12 @@ TTiaraData::~TTiaraData()
 /////////////////////////
 void TTiaraData::Clear()
 {
-  fTiara_StripFront_DetectorNbr.clear();
-  fTiara_StripFront_StripNbr.clear();
-  fTiara_StripFront_Energy.clear();
-  fTiara_StripFront_TimeCFD.clear();
-  fTiara_StripFront_TimeLED.clear();
 
-  fTiara_StripBack_DetectorNbr.clear();
-  fTiara_StripBack_StripNbr.clear();
-  fTiara_StripBack_Energy.clear();
-  fTiara_StripBack_TimeCFD.clear();
-  fTiara_StripBack_TimeLED.clear();
-  
-  fTiara_PAD_DetectorNbr.clear();
-  fTiara_PAD_Energy.clear();
-  fTiara_PAD_TimeCFD.clear();
-  fTiara_PAD_TimeLED.clear();
 
 }
 
 /////////////////////////
 void TTiaraData::Dump() const
 {
-  // Energy
-  cout << "Tiara_StripFront_Mult = " << fTiara_StripFront_DetectorNbr.size() << endl;
-  
-  // Front
-  for (UShort_t i = 0; i < fTiara_StripFront_DetectorNbr.size(); i++){
-    cout << "DetNbr: " << fTiara_StripFront_DetectorNbr[i]
-         << " Strip: " << fTiara_StripFront_StripNbr[i]
-         << " Energy: " << fTiara_StripFront_Energy[i]
-         << " Time CFD: " << fTiara_StripFront_TimeCFD[i]
-         << " Time LED: " << fTiara_StripFront_TimeLED[i] << endl;
-  }
-  
-  // Back
-  for (UShort_t i = 0; i < fTiara_StripFront_DetectorNbr.size(); i++){
-    cout << "DetNbr: " << fTiara_StripFront_DetectorNbr[i]
-    << " Strip: " << fTiara_StripFront_StripNbr[i]
-    << " Energy: " << fTiara_StripFront_Energy[i]
-    << " Time CFD: " << fTiara_StripBack_TimeCFD[i]
-    << " Time LED: " << fTiara_StripBack_TimeLED[i] << endl;
-  }
-  
-  // PAD
-  for (UShort_t i = 0; i < fTiara_PAD_DetectorNbr.size(); i++){
-    cout << "DetNbr: " << fTiara_PAD_DetectorNbr[i]
-    << " Energy: " << fTiara_PAD_Energy[i]
-    << " Time CFD: " << fTiara_PAD_TimeCFD[i]
-    << " Time LED: " << fTiara_PAD_TimeLED[i] << endl;
 
-  }
 }
