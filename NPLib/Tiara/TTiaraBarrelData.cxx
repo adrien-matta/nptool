5eaa3b0d2e977800bcabe87623e363595729a716
adrien-matta <a.matta@surrey.ac.uk>
2013-11-14 17:48:45 +0000

    * Change TTiaraDataBarrel

--- NPLib/Tiara/TTiaraBarrelData.cxx
+++ NPLib/Tiara/TTiaraBarrelData.cxx
@@ -32,25 +32,38 @@ ClassImp(TTiaraBarrelData)
   TTiaraBarrelData::TTiaraBarrelData(){
 
     // Reserve the space for the full array 
-    fTiaraBarrel_Front_Upstream_DetectorNbr.reserve(8*4);         
-    fTiaraBarrel_Front_Upstream_StripNbr.reserve(8*4);         
-    fTiaraBarrel_Front_Upstream_Energy.reserve(8*4);        
-    fTiaraBarrel_Front_Upstream_Time.reserve(8*4);          
+    fTiaraBarrel_FrontUpstreamE_DetectorNbr.reserve(8*4);         
+    fTiaraBarrel_FrontUpstreamE_StripNbr.reserve(8*4);         
+    fTiaraBarrel_FrontUpstreamE_Energy.reserve(8*4);        
 
-    fTiaraBarrel_Front_Downstream_DetectorNbr.reserve(8*4);     
-    fTiaraBarrel_Front_Downstream_StripNbr.reserve(8*4);           
-    fTiaraBarrel_Front_Downstream_Energy.reserve(8*4);          
-    fTiaraBarrel_Front_Downstream_Time.reserve(8*4);            
+    fTiaraBarrel_FrontDownstreamE_DetectorNbr.reserve(8*4);     
+    fTiaraBarrel_FrontDownstreamE_StripNbr.reserve(8*4);           
+    fTiaraBarrel_FrontDownstreamE_Energy.reserve(8*4);          
 
-    fTiaraBarrel_Back_DetectorNbr.reserve(8);                  
-    fTiaraBarrel_Back_Energy.reserve(8);                    
-    fTiaraBarrel_Back_Time.reserve(8);                      
+    fTiaraBarrel_BackE_DetectorNbr.reserve(8);                  
+    fTiaraBarrel_BackE_Energy.reserve(8);                    
 
-    fTiaraBarrel_Outer_DetectorNbr.reserve(8*4);               
-    fTiaraBarrel_Outer_StripNbr.reserve(8*4);                    
-    fTiaraBarrel_Outer_Energy.reserve(8*4);                        
-    fTiaraBarrel_Outer_Time.reserve(8*4);
-  }
+    fTiaraBarrel_OuterE_DetectorNbr.reserve(8*4);               
+    fTiaraBarrel_OuterE_StripNbr.reserve(8*4);                    
+    fTiaraBarrel_OuterE_Energy.reserve(8*4);                        
+  
+    fTiaraBarrel_FrontUpstreamT_DetectorNbr.reserve(8*4);         
+    fTiaraBarrel_FrontUpstreamT_StripNbr.reserve(8*4);         
+    fTiaraBarrel_FrontUpstreamT_Time.reserve(8*4);          
+
+    fTiaraBarrel_FrontDownstreamT_DetectorNbr.reserve(8*4);     
+    fTiaraBarrel_FrontDownstreamT_StripNbr.reserve(8*4);           
+    fTiaraBarrel_FrontDownstreamT_Time.reserve(8*4);            
+
+    fTiaraBarrel_BackT_DetectorNbr.reserve(8);                  
+    fTiaraBarrel_BackT_Time.reserve(8);                      
+
+    fTiaraBarrel_OuterT_DetectorNbr.reserve(8*4);               
+    fTiaraBarrel_OuterT_StripNbr.reserve(8*4);                    
+    fTiaraBarrel_OuterT_Time.reserve(8*4);
+
+
+}
 
 /////////////////////////
 TTiaraBarrelData::~TTiaraBarrelData(){
@@ -59,29 +72,43 @@ TTiaraBarrelData::~TTiaraBarrelData(){
 
 /////////////////////////
 void TTiaraBarrelData::Clear(){
-  fTiaraBarrel_Front_Upstream_DetectorNbr.clear();         
-  fTiaraBarrel_Front_Upstream_StripNbr.clear();         
-  fTiaraBarrel_Front_Upstream_Energy.clear();        
-  fTiaraBarrel_Front_Upstream_Time.clear();          
+  fTiaraBarrel_FrontUpstreamE_DetectorNbr.clear();         
+  fTiaraBarrel_FrontUpstreamE_StripNbr.clear();         
+  fTiaraBarrel_FrontUpstreamE_Energy.clear();        
 
-  fTiaraBarrel_Front_Downstream_DetectorNbr.clear();     
-  fTiaraBarrel_Front_Downstream_StripNbr.clear();           
-  fTiaraBarrel_Front_Downstream_Energy.clear();          
-  fTiaraBarrel_Front_Downstream_Time.clear();            
+  fTiaraBarrel_FrontDownstreamE_DetectorNbr.clear();     
+  fTiaraBarrel_FrontDownstreamE_StripNbr.clear();           
+  fTiaraBarrel_FrontDownstreamE_Energy.clear();          
 
-  fTiaraBarrel_Back_DetectorNbr.clear();                  
-  fTiaraBarrel_Back_Energy.clear();                    
-  fTiaraBarrel_Back_Time.clear();                      
+  fTiaraBarrel_BackE_DetectorNbr.clear();                  
+  fTiaraBarrel_BackE_Energy.clear();                    
 
-  fTiaraBarrel_Outer_DetectorNbr.clear();               
-  fTiaraBarrel_Outer_StripNbr.clear();                    
-  fTiaraBarrel_Outer_Energy.clear();                        
-  fTiaraBarrel_Outer_Time.clear();
+  fTiaraBarrel_OuterE_DetectorNbr.clear();               
+  fTiaraBarrel_OuterE_StripNbr.clear();                    
+  fTiaraBarrel_OuterE_Energy.clear();                        
+  
+  fTiaraBarrel_FrontUpstreamT_DetectorNbr.clear();        
+  fTiaraBarrel_FrontUpstreamT_StripNbr.clear();         
+  fTiaraBarrel_FrontUpstreamT_Time.clear();          
+
+  fTiaraBarrel_FrontDownstreamT_DetectorNbr.clear();     
+  fTiaraBarrel_FrontDownstreamT_StripNbr.clear();           
+  fTiaraBarrel_FrontDownstreamT_Time.clear();            
+
+  fTiaraBarrel_BackT_DetectorNbr.clear();                  
+  fTiaraBarrel_BackT_Time.clear();                      
+
+  fTiaraBarrel_OuterT_DetectorNbr.clear();               
+  fTiaraBarrel_OuterT_StripNbr.clear();                    
+  fTiaraBarrel_OuterT_Time.clear();
+
+
+
 }
 
 /////////////////////////
 void TTiaraBarrelData::Dump() const{
-  cout << "************* TTiaraBarrelData Event ************"<< endl;
+/*  cout << "************* TTiaraBarrelData Event ************"<< endl;
   cout << "*** Inner Barrel Front Upstream ***" << endl ;
   for( unsigned int i = 0; i < fTiaraBarrel_Front_Upstream_DetectorNbr.size(); i ++){
     cout << "Detector Number = " 
@@ -124,6 +151,6 @@ void TTiaraBarrelData::Dump() const{
       << fTiaraBarrel_Outer_Energy[i] << endl ;
     cout << "Time = " 
       << fTiaraBarrel_Outer_Time[i] << endl ;  
-  }
+  }*/
 }
 
