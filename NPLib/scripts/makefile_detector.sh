33e4dbf749f5e61062b239950ed8d46108ca9d64
adrien-matta <a.matta@surrey.ac.uk>
2014-01-31 17:49:07 +0000

    * Improvement in the way the compilation is performed

--- NPLib/scripts/makefile_detector.sh
+++ NPLib/scripts/makefile_detector.sh
@@ -26,21 +26,25 @@
 # *                                                                           *
 # *****************************************************************************
 
-
+#Target name
+lname=$(printf "$1\n"  | tr '[A-Z]' '[a-z]')
+printf  "$lname: " >> $2
+#Dependancies
+printf "FillIncludeDir \n" >> $2
 # build message 
-printf  "\t@echo \"Entering $1 directory...\"" >> $2
+printf  "\t@echo \"Entering $1 directory...\"\n" >> $2
 # execute make command with target specified on command line
-printf  "\tmake --silent -C ./$1" >> $2
+printf  "\tmake --silent -C ./$1\n" >> $2
 # copy header files
-printf  "\tcd $1; cp -f *.h ../include" >> $2
+#printf  "\tcd $1; cp -f *.h ../include\n" >> $2
 # remove *Dict header files
-printf  "\tcd include; rm *Dict.h" >> $2
+#printf  "\tcd include; rm *Dict.h\n" >> $2
 # copy library files
-printf  "\tcd $1; cp -f *.so ../lib" >> $2
+#printf  "\tcd $1; cp -f *.so ../lib\n" >> $2
 # deal with mac osx dylib files
 #echo "ifeq (\$(findstring macosx, \$(ARCH)), macosx)" >> $2
 #echo "\t@echo \"to be done\""
 #echo "endif" >> $2
 # newline
-printf "" >> $2
+printf "\n" >> $2
 
