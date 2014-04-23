335bfc1cf8eda0cfe66242559df9464f13d92937
adrien-matta <a.matta@surrey.ac.uk>
2014-04-23 11:33:44 +0100

    * adding nptool.sh script for easier installation  - Former installation method still working  - user just have to source nptool.sh to setup the environment variable

---
+++ nptool.sh
@@ -0,0 +1,30 @@
+#!bin/bash
+
+# source this file to setup your NPTOOL installation
+CUR_DIR="$PWD" 
+SCRIPTFILE=$0 
+if [ "${SCRIPTFILE}" = "-bash" ] ; then  
+  SCRIPTFILE=${BASH_ARGV[0]} 
+fi 
+
+SCRIPTPATH="${SCRIPTFILE}" 
+
+export NPTOOL=$(dirname $SCRIPTPATH)
+export NPLIB=$NPTOOL/NPLib
+
+NPARCH=$(uname)
+# mac os x case
+if [ "${NPARCH}" = "Darwin" ] ; 
+then
+  export DYLD_LIBRARY_PATH=$DYLD_LIBRARY_PATH:$NPTOOL/NPLib/lib
+else 
+  export LD_LIBRARY_PATH=$LD_LIBRARY_PATH:$NPTOOL/NPLib/lib
+fi
+
+alias NPSimulation='$NPTOOL/NPSimulation/bin/$G4SYSTEM/Simulation'
+alias NPAnalysis='$NPANA/Analysis'
+
+alias NPT='cd $NPTOOL'  
+alias NPL='cd $NPTOOL/NPLib'  
+alias NPS='cd $NPTOOL/NPSimulation'
+alias NPA='cd $NPTOOL/NPAnalysis'
