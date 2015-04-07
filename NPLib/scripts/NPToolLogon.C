1e7a0573995736a104a5e49aebac6615f03d24f1
adrien-matta <a.matta@surrey.ac.uk>
2015-04-07 11:39:10 +0100

    * Changing the NPToolLogon.C to not load the lib if the rootmap exist

--- NPLib/scripts/NPToolLogon.C
+++ NPLib/scripts/NPToolLogon.C
@@ -22,84 +22,103 @@
  *****************************************************************************/
 
 // C++ headers
-#include <iostream>
-#include <vector>
+#include<iostream>
+#include<vector>
+#include<stdlib.h>
+#include<dlfcn.h>
+#include<dirent.h>
 using namespace std;
 
 // ROOT headers
-#include "TSystem.h"
-#include "TROOT.h"
-#include "TList.h"
-#include "TSystemDirectory.h"
-#include "TString.h"
-#include "TRandom.h"
-#include "TRandom2.h"
+#include"TSystem.h"
+#include"TROOT.h"
+#include"TList.h"
+#include"TSystemDirectory.h"
+#include"TString.h"
+#include"TRandom.h"
+#include"TRandom2.h"
 
-void NPToolLogon(bool verbosemode = false)
-{
-   // Change the standard random generator to TRandom2
-   gRandom = new TRandom2();
+void NPToolLogon(bool verbosemode = false){
+  verbosemode = false;
+  // Change the standard random generator to TRandom2
+  gRandom = new TRandom2();
 
-   TString currentpath = gSystem->Getenv("PWD");
-   TString path = gSystem->Getenv("NPTOOL");
-   
-   // Add include path
-   if (verbosemode) cout << "NPTool: adding include path : " << path << "/NPLib/include" << endl;
-   gROOT->ProcessLine(Form(".include %s/NPLib/include", path.Data()));
+  TString currentpath = gSystem->Getenv("PWD");
+  TString path = gSystem->Getenv("NPTOOL");
 
-   // Add shared libraries
-   if (verbosemode) cout << "NPTool: loading NPLib shared libraries ..." << endl;
-   TString libpath = Form("%s/NPLib/lib", path.Data());
-   TSystemDirectory libdir("libdir", libpath);
-   TList* listfile = libdir.GetListOfFiles();
-   
-   // Since the list is ordered alphabetically and that the 
-   // libVDetector.dylib library should be loaded before the 
-   // lib*Physics.dylib libraries, it is then loaded manually 
-   // first.
-   // Test if the lib directory is empty or not
-   if (listfile->GetEntries() > 2) gSystem->Load(libpath+"/libNPCore.dylib");
+  // Add include path
+  gROOT->ProcessLine(Form(".include %s/NPLib/include", path.Data()));
 
-   gSystem->Load("libPhysics.so"); // Needed by Must2 and Sharc
-   gSystem->Load("libHist.so"); // Needed by TSpectra Class
-   gSystem->Load("libCore.so"); // Need by Maya
-   // Loop on Data libraries
-   Int_t i = 0;
-   while (listfile->At(i)) {
+  // Test if the root map exist, 
+  // if yes exit
+  // if no load the nptool lib
+  DIR* dir;
+  struct dirent *ent;
+  TString libpath = path+"/NPLib/lib";
+    string file;
+  bool check = false;
+  if((dir=opendir(libpath))!= NULL){
+    while((ent = readdir(dir))!=NULL){
+      file = ent->d_name;    
+      if(file.find(".rootmap")!=std::string::npos){
+        check=true;
+        break;
+      }
+    }
+  }
+
+  if(!check){
+    // Add shared libraries
+    TString libpath = Form("%s/NPLib/lib", path.Data());
+    TSystemDirectory libdir("libdir", libpath);
+    TList* listfile = libdir.GetListOfFiles();
+
+    // Since the list is ordered alphabetically and that the 
+    // libVDetector.dylib library should be loaded before the 
+    // lib*Physics.dylib libraries, it is then loaded manually 
+    // first.
+    // Test if the lib directory is empty or not
+    if (listfile->GetEntries() > 2) gSystem->Load(libpath+"/libNPCore.dylib");
+
+    gSystem->Load("libPhysics.so"); // Needed by Must2 and Sharc
+    gSystem->Load("libHist.so"); // Needed by TSpectra Class
+    gSystem->Load("libCore.so"); // Need by Maya
+    // Loop on Data libraries
+    Int_t i = 0;
+    while (listfile->At(i)) {
       TString libname = listfile->At(i++)->GetName();
       if (libname.Contains(".dylib") && libname.Contains("Data") && !libname.Contains("libVDetector.dylib")) {
-         TString lib = libpath + "/" + libname;
-         gSystem->Load(lib);
+        TString lib = libpath + "/" + libname;
+        gSystem->Load(lib);
       }
-   }
-	  
-   // Loop on Physics Library
-   i = 0;
-   while (listfile->At(i)) {
+    }
+
+    // Loop on Physics Library
+    i = 0;
+    while (listfile->At(i)) {
       TString libname = listfile->At(i++)->GetName();
       if (libname.Contains(".dylib") && libname.Contains("Physics") &&!libname.Contains("libVDetector.dylib")) {
-         TString lib = libpath + "/" + libname;
-         gSystem->Load(lib);
+        TString lib = libpath + "/" + libname;
+        gSystem->Load(lib);
       }
-   }
- 
-   // Loop on the Reset of the Library
-   i = 0;
-   while (listfile->At(i)) {
+    }
+
+    // Loop on the Reset of the Library
+    i = 0;
+    while (listfile->At(i)) {
       TString libname = listfile->At(i++)->GetName();
       if (libname.Contains(".dylib") && !libname.Contains("Physics") && !libname.Contains("Data")  &&!libname.Contains("libVDetector.dylib")) {
-         TString lib = libpath + "/" + libname;
-         gSystem->Load(lib);
+        TString lib = libpath + "/" + libname;
+        gSystem->Load(lib);
       }
-   }
+    }
 
 
-  // gROOT->ProcessLine(".L $NPTOOL/NPLib/include/RootInput.h+");   
- 
-   // Since the libdir.GetListOfFiles() commands cds to the
-   // libidr directory, one has to return to the initial
-   // directory
-   gSystem->cd(currentpath);
-   
-   if (verbosemode) cout << "NPTool: Ready" << endl;
+    // gROOT->ProcessLine(".L $NPTOOL/NPLib/include/RootInput.h+");   
+
+    // Since the libdir.GetListOfFiles() commands cds to the
+    // libidr directory, one has to return to the initial
+    // directory
+    gSystem->cd(currentpath);
+  }
 }
