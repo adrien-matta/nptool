a70ab3e3adab0807810b683af4bac9e70495e6ef
adrien-matta <a.matta@surrey.ac.uk>
2015-04-07 16:24:29 +0100

    * Fixing npanalysis

--- NPLib/Utility/npanalysis.cxx
+++ NPLib/Utility/npanalysis.cxx
@@ -8,7 +8,9 @@
 
 // STL
 #include<iostream>
-#include <dlfcn.h>
+#include<stdio.h>
+#include<dlfcn.h>
+#include<stdlib.h>
 
 #ifndef SHARED_LIB_EXTENSION_GLOB
 #define SHARED_LIB_EXTENSION_GLOB
@@ -25,10 +27,12 @@ std::string SHARED_LIB_EXTENSION = ".dylib";
 #endif
 #endif
 
+void ProgressDisplay(clock_t&,clock_t&,unsigned int&, unsigned int&, int&);
+
 int main(int argc , char** argv){
   // command line parsing
   NPOptionManager* myOptionManager = NPOptionManager::getInstance(argc,argv);
-  
+
   // if input files are not given, use those from TAsciiFile
   if (myOptionManager->IsDefault("DetectorConfiguration")) {
     string name = RootInput::getInstance()->DumpAsciiFile("DetectorConfiguration");
@@ -42,7 +46,7 @@ int main(int argc , char** argv){
   // Instantiate RootOutput
   RootOutput::getInstance("Analysis/"+OutputfileName, "ResultTree");
   TTree* tree= RootOutput::getInstance()->GetTree();
-  
+
   // Instantiate the detector using a file
   NPA::DetectorManager* myDetector = new DetectorManager();
   myDetector->ReadConfigurationFile(detectorfileName);
@@ -66,9 +70,14 @@ int main(int argc , char** argv){
     nentries = myOptionManager->GetNumberOfEntryToAnalyse() ;
   std::cout << " Number of Event to be treated : " << nentries << std::endl;
 
+  clock_t begin = clock();
+  clock_t end = begin;
+  unsigned int treated = 0;
+  unsigned int inter = 0;
 
   if(UserAnalysis==NULL){ 
-    for (int i = 0 ; i < nentries; i++) { 
+    for (unsigned int i = 0 ; i < nentries; i++) { 
+      ProgressDisplay(begin,end,treated,inter,nentries);
       // Get the raw Data
       Chain -> GetEntry(i);
       // Clear previous Event
@@ -81,7 +90,8 @@ int main(int argc , char** argv){
   }
 
   else{
-    for (int i = 0 ; i < nentries; i++) { 
+    for (unsigned int i = 0 ; i < nentries; i++) { 
+      ProgressDisplay(begin,end,treated,inter,nentries);
       // Get the raw Data
       Chain -> GetEntry(i);
       // Clear previous Event
@@ -95,7 +105,29 @@ int main(int argc , char** argv){
     }
     UserAnalysis->End();
   }
- 
+
+  ProgressDisplay(begin,end,treated,inter,nentries);
   RootOutput::getInstance()->Destroy();
   return 0;
+}
+
+////////////////////////////////////////////////////////////////////////////////
+void ProgressDisplay(clock_t& begin, clock_t& end, unsigned int& treated,unsigned int& inter,int& total){
+  end = clock();
+  if((end-begin)/CLOCKS_PER_SEC>CLOCKS_PER_SEC||treated>=total ){
+  long double elapsed =(long double) (end-begin)/CLOCKS_PER_SEC;
+    double event_rate = inter/elapsed;
+    double percent = 100*treated/total;
+    double remain = (total-treated)/event_rate;
+    if(treated!=total)
+      printf("\r \033[1;31m ******* Progress: %.1f | Rate: %.1f evt/s | Remain: %.1fs *******\033[0m", percent,event_rate,remain);
+    else
+      printf("\r \033[1;32m ******* Progress: %.1f | Rate: %.1f evt/s | Remain: %.1fs *******\033[0m", percent,event_rate,remain);
+    
+    fflush(stdout);
+    begin = clock() ;
+    inter=0;
+  }
+ treated++; 
+ inter++;
 }
