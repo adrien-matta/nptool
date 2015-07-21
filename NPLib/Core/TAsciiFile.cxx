3396e025bb59182bc5b96edea9fbe58471fa8361
adrien-matta <a.matta@surrey.ac.uk>
2015-07-21 09:58:20 +0100

    * Cleaning up TASCII.cxx

--- NPLib/Core/TAsciiFile.cxx
+++ NPLib/Core/TAsciiFile.cxx
@@ -30,106 +30,91 @@ using namespace std;
 
 ClassImp(TAsciiFile)
 
-
-TAsciiFile::TAsciiFile()
-   : TNamed()
-{
-}
+  ////////////////////////////////////////////////////////////////////////////////
+  TAsciiFile::TAsciiFile():TNamed(){
+  }
 
 
-
+////////////////////////////////////////////////////////////////////////////////
 TAsciiFile::TAsciiFile(const char* name, const char* title, const char* inputAsciiFile)
-   : TNamed(name, title)
-{
-   ReadFile(inputAsciiFile);
+  : TNamed(name, title){
+    ReadFile(inputAsciiFile);
+  }
+
+
+////////////////////////////////////////////////////////////////////////////////
+TAsciiFile::~TAsciiFile(){
 }
 
 
+////////////////////////////////////////////////////////////////////////////////
+void TAsciiFile::ReadFile(const char* inputAsciiFile){
+  ifstream inputStream(inputAsciiFile);
+  if (!inputStream) {
+    cout << "Problem opening file " << inputAsciiFile << endl;
+  }
+  else {
+    string line;
+    while (!inputStream.eof()) {
+      getline(inputStream, line);
+      fLines.push_back(line);
+    }
+  }
 
-TAsciiFile::~TAsciiFile()
-{
+  inputStream.close();
 }
 
 
-
-void TAsciiFile::ReadFile(const char* inputAsciiFile)
-{
-   ifstream inputStream(inputAsciiFile);
-   if (!inputStream) {
-      cout << "Problem opening file " << inputAsciiFile << endl;
-   }
-   else {
-      string line;
-      while (!inputStream.eof()) {
-         getline(inputStream, line);
-         fLines.push_back(line);
-      }
-   }
-
-   inputStream.close();
+////////////////////////////////////////////////////////////////////////////////
+bool TAsciiFile::IsEmpty() const{
+  if (fLines.size() == 0) return true;
+  else return false;
 }
 
 
-
-bool TAsciiFile::IsEmpty() const
-{
-   if (fLines.size() == 0) return true;
-   else return false;
+////////////////////////////////////////////////////////////////////////////////
+void TAsciiFile::Append(const char* inputAsciiFile){
+  ReadFile(inputAsciiFile);
 }
 
-
-
-void TAsciiFile::Append(const char* inputAsciiFile)
-{
-   ReadFile(inputAsciiFile);
-}
-
-
-void TAsciiFile::AppendLine(const char* AsciiLine)
-{
+////////////////////////////////////////////////////////////////////////////////
+void TAsciiFile::AppendLine(const char* AsciiLine){
   string line = AsciiLine ;
-  
   fLines.push_back(line);
 }
 
+////////////////////////////////////////////////////////////////////////////////
+void TAsciiFile::WriteToFile(const char* outputAsciiFile) const{
+  ofstream outputStream(outputAsciiFile);
 
-void TAsciiFile::WriteToFile(const char* outputAsciiFile) const
-{
-   ofstream outputStream(outputAsciiFile);
-
-   for (UInt_t i = 0; i < fLines.size(); i++) outputStream << fLines[i] << endl;
-
-   outputStream.close();
+  for (UInt_t i = 0; i < fLines.size(); i++) outputStream << fLines[i] << endl;
+  outputStream.close();
 }
 
 
+////////////////////////////////////////////////////////////////////////////////
+void TAsciiFile::WriteToFile(const char* outputAsciiFile, UInt_t begin, UInt_t end) const{
+  ofstream outputStream(outputAsciiFile);
+  for (UInt_t i = 0; i < fLines.size(); i++) {
+    if (i < begin || i > end) continue;
+    outputStream << fLines[i] << endl;
+  }
 
-void TAsciiFile::WriteToFile(const char* outputAsciiFile, UInt_t begin, UInt_t end) const
-{
-   ofstream outputStream(outputAsciiFile);
-
-   for (UInt_t i = 0; i < fLines.size(); i++) {
-      if (i < begin || i > end) continue;
-      outputStream << fLines[i] << endl;
-   }
-
-   outputStream.close();
+  outputStream.close();
 }
 
 
-
-void TAsciiFile::Print() const
-{
-   for (UInt_t i = 0; i < fLines.size(); i++) cout << fLines[i] << endl;
+////////////////////////////////////////////////////////////////////////////////
+void TAsciiFile::Print() const{
+  for (UInt_t i = 0; i < fLines.size(); i++) cout << fLines[i] << endl;
 }
 
 
-
-void TAsciiFile::Print(UInt_t begin, UInt_t end) const
-{
-   for (UInt_t i = 0; i < fLines.size(); i++) {
-      if (i < begin || i > end) continue;
-      cout << fLines[i] << endl;
-   }
+////////////////////////////////////////////////////////////////////////////////
+void TAsciiFile::Print(UInt_t begin, UInt_t end) const{
+  for (UInt_t i = 0; i < fLines.size(); i++) {
+    if (i < begin || i > end) continue;
+    cout << fLines[i] << endl;
+  }
 }
 
