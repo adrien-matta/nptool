54e91df2131fb14ffef6b908be9f63e4a4dee547
adrien-matta <a.matta@surrey.ac.uk>
2015-07-29 12:37:16 +0100

    * adding missing script

---
+++ NPLib/scripts/test/basic.sh
@@ -0,0 +1,36 @@
+#!/bin/bash
+
+# exit on error, and verbose
+set -ev
+
+# can run the app
+npanalysis -h
+
+# Compile Example1 
+cd ../NPAnalysis/Example1
+cmake ./
+if [ -e Makefile ] 
+then
+  make
+else
+  ninja
+fi
+# Moving reference test simulation
+cp $NPTOOL/Examples/Example1_Simu.root $NPTOOL/Outputs/Simulation/Example1.root
+npanalysis -R RunToTreat.txt -O Example1
+
+echo ".q" | root -l ShowResult.cxx
+
+# Compile Example2
+cd ../Example2
+cmake ./
+if [ -e Makefile ] 
+then
+  make
+else
+  ninja
+fi
+cp $NPTOOL/Examples/Example2_Simu.root $NPTOOL/Outputs/Simulation/Example2.root
+npanalysis -R RunToTreat.txt -O Example2
+echo ".q" | root -l ShowResult.cxx
+
