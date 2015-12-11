c6922545b325b317552eb40c09a5f0849ec9c347
adrien-matta <a.matta@surrey.ac.uk>
2015-12-11 18:22:08 +0000

    * Updating benchmark.sh

--- Benchmarks/benchmark.sh
+++ Benchmarks/benchmark.sh
@@ -3,10 +3,7 @@
 # exit on error, and verbose
 set -ev
 
-# can run the app
-npanalysis -h
-
 # cats benchmark 
 cd $NPTOOL/Benchmarks/cats
-npanalysis -D benchmark_cats.detector -C calibration.txt -R RunToTreat.txt -O benchmark_cats 
+time npanalysis -D benchmark_cats.detector -C calibration.txt -R RunToTreat.txt -O benchmark_cats 
 echo ".q" | root -b -l ShowResult.cxx
