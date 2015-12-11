c093fc186b8562b4c1343f3270a012baa7124eff
adrien-matta <a.matta@surrey.ac.uk>
2015-12-11 18:19:04 +0000

    * Fixing cats benchmarks macro

--- Benchmarks/benchmark.sh
+++ Benchmarks/benchmark.sh
@@ -9,4 +9,4 @@ npanalysis -h
 # cats benchmark 
 cd $NPTOOL/Benchmarks/cats
 npanalysis -D benchmark_cats.detector -C calibration.txt -R RunToTreat.txt -O benchmark_cats 
-echo ".q" | root -b -l ShowResult.C
+echo ".q" | root -b -l ShowResult.cxx
