635fd1c4457329279c75fafbabc5c3f5bca62a5e
Nicolas de Sereville <deserevi@ipno.in2p3.fr>
2016-01-05 11:39:57 +0100

    + Remove occurence to InitialConditions and InteractionCoordinates

--- NPLib/Physics/NPPhysicsLinkDef.h
+++ NPLib/Physics/NPPhysicsLinkDef.h
@@ -3,6 +3,4 @@
 #pragma link C++ defined_in "./NPBeam.h";
 #pragma link C++ defined_in "./NPNucleus.h";
 #pragma link C++ defined_in "./NPEnergyLoss.h";
-#pragma link C++ defined_in "./TInitialConditions.h";
-#pragma link C++ defined_in "./TInteractionCoordinates.h";
 #endif
