9bfa66e7326edcec8c58ec359eef75e2efee1bde
Nicolas de Sereville <deserevi@ipno.in2p3.fr>
2016-01-05 11:17:50 +0100

    + Move InitialConditions and InteractionCoordinates class in the   Physics module

--- NPLib/Physics/NPPhysicsLinkDef.h
+++ NPLib/Physics/NPPhysicsLinkDef.h
@@ -3,4 +3,6 @@
 #pragma link C++ defined_in "./NPBeam.h";
 #pragma link C++ defined_in "./NPNucleus.h";
 #pragma link C++ defined_in "./NPEnergyLoss.h";
+#pragma link C++ defined_in "./TInitialConditions.h";
+#pragma link C++ defined_in "./TInteractionCoordinates.h";
 #endif
