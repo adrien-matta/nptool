9742d3cfe3e06bfe8feb912e4bc0fcd75f5f4b1e
adrien-matta <a.matta@surrey.ac.uk>
2016-06-30 14:58:14 -0500

    * Adding Results.cxx macro to T40 project

---
+++ Projects/T40/Results.cxx
@@ -0,0 +1,11 @@
+
+
+void Results(){
+  TFile* File = new TFile("../../Outputs/Analysis/PhysicsTree.root");
+  TTree* Tree = (TTree*) File->FindObjectAny("PhysicsTree");
+
+  Tree->Draw("ELab:ThetaLab>>h2(1000,0,180,1000,0,20)","ELab>0","colz");
+  new TCanvas();
+  Tree->Draw("Ex>>h(1000,-5,5)","ELab>0&& ThetaLab>105 && TiaraHyball.Strip_E@.size()>0");
+
+}
