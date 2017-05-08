bab81ad259aadee63b201f3e03af54519007a462
local1 <local1@localhost.localdomain>
2017-05-08 16:38:54 +0200

    modified Chamber.cc and .hh for MuGasT gdml chamber

--- NPSimulation/Core/Chamber.hh
+++ NPSimulation/Core/Chamber.hh
@@ -1,7 +1,7 @@
 #ifndef Chamber2Array_h
 #define Chamber2Array_h 1
 /*****************************************************************************
- * Copyright (C) 2009-2016   this file is part of the NPTool Project         *
+ * Copyright (C) 2009   this file is part of the NPTool Project              *
  *                                                                           *
  * For the licensing terms see $NPTOOL/Licence/NPTool_Licence                *
  * For the list of contributors see $NPTOOL/Licence/Contributors             *
@@ -11,14 +11,14 @@
  * Original Author: M. Labiche    contact address: marc.labiche@atfc.ac.uk   *
  *                                                                           *
  * Creation Date  : January 2010                                             *
- * Last update    : 19/07/2016                                               *
+ * Last update    : 11/03/2010                                               *
  *---------------------------------------------------------------------------*
  * Decription:                                                               *
  *  This class describe Cryogenic and standard Chamber.                      *
- *  Derived fromNPS::VDetector                                               *
+ *  Derived from VDetector                                                   *
  *---------------------------------------------------------------------------*
  * Comment:                                                                  *
- *                                                                           *
+ *
  *                                                                           *
  *****************************************************************************/
 // C++ headers
@@ -37,90 +37,100 @@
 #include "G4UnionSolid.hh"
 #include "G4IntersectionSolid.hh"
 
-
 // NPTool headers
 #include "NPSVDetector.hh"
 #include "NPInputParser.h"
+
 using namespace std;
 using namespace CLHEP;
 
 class Chamber : public NPS::VDetector
 {
-   public:
-      Chamber();
-      ~Chamber(){};
+	public:
+	   Chamber();
+	   ~Chamber(){};
 
 
-   public:
-      //   Read stream at Configfile to pick-up parameters of detector (Position,...)
-      //   Called in DetecorConstruction::ReadDetextorConfiguration Method
-      void ReadConfiguration(NPL::InputParser);
+	public:
+		//	Read stream at Configfile to pick-up parameters of detector (Position,...)
+		//	Called in DetecorConstruction::ReadDetextorConfiguration Method
+		//void ReadConfiguration(string Path);
+      		void ReadConfiguration(NPL::InputParser);
 
-      //   Construct detector and inialise sensitive part.
-      //   Called After DetecorConstruction::AddDetector Method
-      void ConstructDetector(G4LogicalVolume* world);
+		//	Construct detector and inialise sensitive part.
+		//	Called After DetecorConstruction::AddDetector Method
+		void ConstructDetector(G4LogicalVolume* world);
 
-      //   Add Detector branch to the EventTree.
-      //   Called After DetecorConstruction::AddDetector Method
-      void InitializeRootOutput();
+		//	Add Detector branch to the EventTree.
+		//	Called After DetecorConstruction::AddDetector Method
+		void InitializeRootOutput();
 
-      //   Read sensitive part and fill the Root tree.
-      //   Called at in the EventAction::EndOfEventAvtion
-      void ReadSensitive(const G4Event* event);
-
-
-   public:
-      //   Return Material from the Chamber Material Library
-      G4Material* GetMaterialFromLibrary(G4String MaterialName, G4double Temperature = 0, G4double Pressure = 0);
-      
-      //   Generate a DEDX file table using the material used in the target
-      void             WriteDEDXTable(G4ParticleDefinition* Particle,G4double Emin,G4double Emax);
-
-   public:
-      G4double    GetChamberRmin()     {return m_ChamberRmin;}
-      G4double    GetChamberRmax()     {return m_ChamberRmax;}
-      G4double    GetChamberPhiMin()   {return m_ChamberPhiMin;}
-      G4double    GetChamberPhiMax()   {return m_ChamberPhiMax;}
-      G4double    GetChamberThetaMin() {return m_ChamberThetaMin;}
-      G4double    GetChamberThetaMax() {return m_ChamberThetaMax;}
-      G4Material* GetChamberMaterial() {return m_ChamberMaterial;}
-  //G4int       GetChamberNbLayers()      {return m_ChamberNbLayers;}
+		//	Read sensitive part and fill the Root tree.
+		//	Called at in the EventAction::EndOfEventAvtion
+		void ReadSensitive(const G4Event* event);
 
 
-   private:
-      // Chamber type : true = normal ; false = cryo
-      //bool     m_ChamberType;
-      G4int     m_ChamberType;
+	public:
+		//	Return Material from the Chamber Material Library
+		G4Material* GetMaterialFromLibrary(G4String MaterialName, G4double Temperature = 0, G4double Pressure = 0);
+		
+		//	Generate a DEDX file table using the material used in the target
+		void 				WriteDEDXTable(G4ParticleDefinition* Particle,G4double Emin,G4double Emax);
 
-      // Standard parameter
-      G4double    m_ChamberRmin;
-      G4double    m_ChamberRmax;
-      G4double    m_ChamberPhiMin;
-      G4double    m_ChamberPhiMax;
-      G4double    m_ChamberThetaMin;
-      G4double    m_ChamberThetaMax;
-      G4Material* m_ChamberMaterial;
-      G4int       m_ChamberNbLayers;
+	public:
+		G4double    GetChamberRmin()	{return m_ChamberRmin;}
+		G4double    GetChamberRmax()			{return m_ChamberRmax;}
+		G4double    GetChamberPhiMin()			{return m_ChamberPhiMin;}
+		G4double    GetChamberPhiMax()			{return m_ChamberPhiMax;}
+		G4double    GetChamberThetaMin()		{return m_ChamberThetaMin;}
+		G4double    GetChamberThetaMax()		{return m_ChamberThetaMax;}
+		G4Material* GetChamberMaterial()		{return m_ChamberMaterial;}
+  //G4int       GetChamberNbLayers()		{return m_ChamberNbLayers;}
+
+
+	private:
+		// Chamber type : true = normal ; false = cryo
+		//bool     m_ChamberType;
+                // Chamber type : 0=normal ; 1=cryo ; 2 = GREAT
+		G4int     m_ChamberType;
+
+		// Standard parameter
+		G4double    m_ChamberRmin;
+		G4double    m_ChamberRmax;
+		G4double    m_ChamberPhiMin;
+		G4double    m_ChamberPhiMax;
+		G4double    m_ChamberThetaMin;
+		G4double    m_ChamberThetaMax;
+		G4Material* m_ChamberMaterial;
+		G4int       m_ChamberNbLayers;
+
+		// GREAT chamber parameter
+		G4double    m_ChamberHmin;
+		G4double    m_ChamberHmax;
+		G4double    m_ChamberWmin;
+		G4double    m_ChamberWmax;
+		G4double    m_ChamberDmin;
+		G4double    m_ChamberDmax;
+
+		// MARA chamber parameter
+		G4LogicalVolume* m_LogicalGDML;
+
+		G4String m_GDMLPath;
+		G4String m_GDMLName;
+		G4String m_GDMLWorld;
+
   /*
-      // For Cryo Chamber
-      G4double    m_ChamberTemperature;
-      G4double    m_ChamberPressure;
-      G4double    m_WindowsThickness;
-      G4Material* m_WindowsMaterial;
+		// For Cryo Chamber
+		G4double    m_ChamberTemperature;
+		G4double    m_ChamberPressure;
+		G4double    m_WindowsThickness;
+		G4Material* m_WindowsMaterial;
 
-      // Positioning
-      G4double    m_ChamberX;
-      G4double    m_ChamberY;
-      G4double    m_ChamberZ;
+		// Positioning
+		G4double    m_ChamberX;
+		G4double    m_ChamberY;
+		G4double    m_ChamberZ;
   */
-
-  // GREAT chamber parameter
-        G4double    m_ChamberHmin;
-       	G4double    m_ChamberHmax;
-       	G4double    m_ChamberWmin;
-       	G4double    m_ChamberWmax;
-       	G4double    m_ChamberDmin;
-       	G4double    m_ChamberDmax;
 
   /* Karl's geometry of GREAT: */
 
@@ -367,8 +377,6 @@ private:
 
   void Place_PhaseII_LookingUpstream(G4LogicalVolume* world);
   void Place_GREAT_LookingUpstream(G4LogicalVolume* world);
-
-
 
 
 };
