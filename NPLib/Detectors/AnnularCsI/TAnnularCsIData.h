3bbeaa03b090bb1bf980f3d30462f5524352211e
Greg <gchristian@tamu.edu>
2018-03-03 22:27:56 -0600

    Aesthetic changes to AnnularCsIData.h only

--- NPLib/Detectors/AnnularCsI/TAnnularCsIData.h
+++ NPLib/Detectors/AnnularCsI/TAnnularCsIData.h
@@ -33,29 +33,29 @@ class TAnnularCsIData : public TObject {
   //////////////////////////////////////////////////////////////
   // data members are hold into vectors in order 
   // to allow multiplicity treatment
-  private: 
-    // Energy
-    vector<UShort_t>   fAnnularCsI_E_DetectorNbr;
-    vector<Double_t>   fAnnularCsI_Energy;
+private: 
+	// Energy
+	vector<UShort_t>   fAnnularCsI_E_DetectorNbr;
+	vector<Double_t>   fAnnularCsI_Energy;
 
-    // Time
-    vector<UShort_t>   fAnnularCsI_T_DetectorNbr;
-    vector<Double_t>   fAnnularCsI_Time;
+	// Time
+	vector<UShort_t>   fAnnularCsI_T_DetectorNbr;
+	vector<Double_t>   fAnnularCsI_Time;
 
 
   //////////////////////////////////////////////////////////////
   // Constructor and destructor
-  public: 
-    TAnnularCsIData();
-    ~TAnnularCsIData();
+public: 
+	TAnnularCsIData();
+	~TAnnularCsIData();
     
 
   //////////////////////////////////////////////////////////////
   // Inherited from TObject and overriden to avoid warnings
-  public:
-    void Clear();
-    void Clear(const Option_t*) {};
-    void Dump() const;
+public:
+	void Clear();
+	void Clear(const Option_t*) {};
+	void Dump() const;
 
 
   //////////////////////////////////////////////////////////////
@@ -63,37 +63,37 @@ class TAnnularCsIData : public TObject {
   // Prefer inline declaration to avoid unnecessary called of 
   // frequently used methods
   // add //! to avoid ROOT creating dictionnary for the methods
-  public:
-    //////////////////////    SETTERS    ////////////////////////
-    // Energy
-    inline void SetEnergy(const UShort_t& DetNbr,const Double_t& Energy){
-      fAnnularCsI_E_DetectorNbr.push_back(DetNbr);
-      fAnnularCsI_Energy.push_back(Energy);
-    };//!
+public:
+	//////////////////////    SETTERS    ////////////////////////
+	// Energy
+	inline void SetEnergy(const UShort_t& DetNbr,const Double_t& Energy){
+		fAnnularCsI_E_DetectorNbr.push_back(DetNbr);
+		fAnnularCsI_Energy.push_back(Energy);
+	};//!
 
     // Time
-    inline void SetTime(const UShort_t& DetNbr,const Double_t& Time)	{
-      fAnnularCsI_T_DetectorNbr.push_back(DetNbr);     
-      fAnnularCsI_Time.push_back(Time);
-    };//!
+	inline void SetTime(const UShort_t& DetNbr,const Double_t& Time)	{
+		fAnnularCsI_T_DetectorNbr.push_back(DetNbr);     
+		fAnnularCsI_Time.push_back(Time);
+	};//!
 
 
     //////////////////////    GETTERS    ////////////////////////
     // Energy
-    inline UShort_t GetMultEnergy() const
-      {return fAnnularCsI_E_DetectorNbr.size();}
-    inline UShort_t GetE_DetectorNbr(const unsigned int &i) const 
-      {return fAnnularCsI_E_DetectorNbr[i];}//!
-    inline Double_t Get_Energy(const unsigned int &i) const 
-      {return fAnnularCsI_Energy[i];}//!
+	inline UShort_t GetMultEnergy() const
+		{return fAnnularCsI_E_DetectorNbr.size();}
+	inline UShort_t GetE_DetectorNbr(const unsigned int &i) const 
+		{return fAnnularCsI_E_DetectorNbr[i];}//!
+	inline Double_t Get_Energy(const unsigned int &i) const 
+		{return fAnnularCsI_Energy[i];}//!
 
-    // Time
-    inline UShort_t GetMultTime() const
-      {return fAnnularCsI_T_DetectorNbr.size();}
-    inline UShort_t GetT_DetectorNbr(const unsigned int &i) const 
-      {return fAnnularCsI_T_DetectorNbr[i];}//!
-    inline Double_t Get_Time(const unsigned int &i) const 
-      {return fAnnularCsI_Time[i];}//!
+	// Time
+	inline UShort_t GetMultTime() const
+		{return fAnnularCsI_T_DetectorNbr.size();}
+	inline UShort_t GetT_DetectorNbr(const unsigned int &i) const 
+		{return fAnnularCsI_T_DetectorNbr[i];}//!
+	inline Double_t Get_Time(const unsigned int &i) const 
+		{return fAnnularCsI_Time[i];}//!
 
 
   //////////////////////////////////////////////////////////////
@@ -102,3 +102,6 @@ class TAnnularCsIData : public TObject {
 };
 
 #endif
+/* Local Variables:  */
+/* mode: c++         */
+/* End:              */
