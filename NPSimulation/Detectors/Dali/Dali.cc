d91dbe8e18a23d852132903c267522eb1c99e08c
Elidiano Tronchin <elidiano.tronchin@gmail.com>
2019-01-04 12:41:12 +0100

    * Changed Resolution, reference to CYRIC tests

--- NPSimulation/Detectors/Dali/Dali.cc
+++ NPSimulation/Detectors/Dali/Dali.cc
@@ -73,7 +73,7 @@ namespace Dali_NS{
   // Energy and time Resolution
   const double EnergyThreshold = 0*MeV;
   const double ResoTime = 0.0*ns; //4.5*ns ;
-  const double ResoEnergy = 0.001*MeV ;
+  const double ResoEnergy = 1.36*MeV ; // mean Resolution(FWHM) 1.7% of 80MeV from slides 20170214-SAMURAI34-setup-DALI.pdf  // 0.001*MeV ;
   const double Radius = 50*mm ; 
   const double Width = 49.76*mm ;
   const double Hight = 84.81*mm ;
