c957b56e78eb7129c702519173eeda94dfdfd6b5
Elidiano Tronchin <elidiano.tronchin@gmail.com>
2019-01-14 16:37:37 +0100

    * Changed Resolution and commented value transparency in square and cyli vis

--- NPSimulation/Detectors/Dali/Dali.cc
+++ NPSimulation/Detectors/Dali/Dali.cc
@@ -73,7 +73,7 @@ namespace Dali_NS{
   // Energy and time Resolution
   const double EnergyThreshold = 0*MeV;
   const double ResoTime = 0.0*ns; //4.5*ns ;
-  const double ResoEnergy = 1.36*MeV ; // mean Resolution(FWHM) 1.7% of 80MeV from slides 20170214-SAMURAI34-setup-DALI.pdf  // 0.001*MeV ;
+  const double ResoEnergy = 0.51*MeV ; // mean Resolution(FWHM) 1.7% of 80MeV from slides 20170214-SAMURAI34-setup-DALI.pdf if 1.7% of 30MeV = 0.51 MeV // 0.001*MeV ;
   const double Radius = 50*mm ; 
   const double Width = 49.76*mm ;
   const double Hight = 84.81*mm ;
@@ -96,8 +96,8 @@ Dali::Dali(){
   Logic_ArrayDali_1 =0;
   
   // RGB Color + Transparency
-  m_VisSquare = new G4VisAttributes(G4Colour(0, 1, 1, 0.3));   
-  m_VisCylinder = new G4VisAttributes(G4Colour(0, 0, 1, 0.3));   
+  m_VisSquare = new G4VisAttributes(G4Colour(0, 1, 1/*, 0.3*/));   
+  m_VisCylinder = new G4VisAttributes(G4Colour(0, 0, 1/*, 0.3*/));   
 
 }
 
